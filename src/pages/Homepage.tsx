eb3cdd5010692317e78cd742c8a0f4bae21198d4
rmbprui <rmbp.rui@gmail.com>
2023-01-31 12:06:04 +0000

    Rearranging models & GitHub page

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,29 @@
 import { Container, Grid, Paper, Typography } from '@mui/material';
 import { styled } from '@mui/system';
+import { GitHub } from '@mui/icons-material';
 
 // Components
 import Layout from '../components/Layout';
 import ToolModel from '../components/ToolModel';
+
+const models = [
+  {
+    name: 'Three JS',
+    model: './models/threejs.glb',
+  },
+  {
+    name: 'React',
+    model: './models/react.glb',
+  },
+  {
+    name: 'Node JS',
+    model: './models/nodejs.glb',
+  },
+  {
+    name: 'Javascript',
+    model: './models/javascript.glb',
+  },
+];
 
 const StyledTool = styled(Paper)(({ theme }) => ({
   display: 'flex',
@@ -19,43 +39,52 @@ const StyledTool = styled(Paper)(({ theme }) => ({
 const Homepage = () => {
   return (
     <Layout footer>
-      <Container style={{ flex: 1 }}>
+      <Container
+        style={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          padding: 4,
+        }}
+      >
         <Typography variant="h1" align="center">
-          Rui Pereira
+          Rui Pereira{' '}
+          <a
+            href="https://github.com/rmbprui"
+            target="_blank"
+            rel="noreferrer"
+            className="github-link"
+          >
+            <GitHub fontSize="inherit" />
+          </a>
         </Typography>
         <Typography variant="h5" align="center">
           Currently experimenting with:
         </Typography>
         <Grid
           container
-          spacing={2}
+          spacing={1}
           justifyContent="center"
-          style={{ marginTop: 0 }}
+          style={{ marginTop: 0, flex: 1, alignItems: 'center' }}
         >
-          <Grid item xs={12} sm={6} md={4} p={4}>
-            <StyledTool elevation={3}>
-              <Typography variant="body1">Three JS</Typography>
-              <ToolModel model="./models/threejs.glb" />
-            </StyledTool>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} p={4}>
-            <StyledTool elevation={3}>
-              <Typography variant="body1">React</Typography>
-              <ToolModel model="./models/react.glb" />
-            </StyledTool>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} p={4}>
-            <StyledTool elevation={3}>
-              <Typography variant="body1">Node JS</Typography>
-              <ToolModel model="./models/nodejs.glb" />
-            </StyledTool>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} p={4}>
-            <StyledTool elevation={3}>
-              <Typography variant="body1">Javascript</Typography>
-              <ToolModel model="./models/javascript.glb" />
-            </StyledTool>
-          </Grid>
+          {models.map((model) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              p={4}
+              sx={{ textAlign: 'center' }}
+            >
+              <StyledTool elevation={3}>
+                <ToolModel model={model.model} />
+              </StyledTool>
+              <Typography variant="h6" fontWeight={600} mt={2}>
+                {model.name}
+              </Typography>
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </Layout>
